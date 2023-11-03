5e955320808b0c4575eb852180c4f1f0ef6c1db0
weboffools <eliot@weboffools.com>
2023-11-03 17:50:16 -0600

    Add divs and style and 3 initial books

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 const btn = document.querySelector('button')
 const display = document.querySelector('.display')
 
+let books = [
+  {
+    'title': 'Republic',
+    'author': 'Plato',
+    'pages': 489
+  },
+  {
+    'title': 'Politics',
+    'author': 'Aristotle',
+    'pages': 298
+  },
+  {
+    'title': 'The Prince',
+    'author': 'Machiavelli',
+    'pages': 222
+  }
+]
