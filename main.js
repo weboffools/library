b6322cd71354707fb4b9843668dffb2612561be3
weboffools <eliot@weboffools.com>
2023-11-03 18:40:54 -0600

    Add function to add books to shelf with DOM manipulation

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 const btn = document.querySelector('button')
-const display = document.querySelector('.display')
+const shelf1 = document.querySelector('.s-1')
+const shelf2 = document.querySelector('.s-2')
+const shelf3 = document.querySelector('.s-3')
+
 
 let books = [
   {
-    'title': 'Republic',
-    'author': 'Plato',
-    'pages': 489
+    title: 'Republic',
+    author: 'Plato',
+    pages: 489
   },
   {
-    'title': 'Politics',
-    'author': 'Aristotle',
-    'pages': 298
+    title: 'Politics',
+    author: 'Aristotle',
+    pages: 298
   },
   {
-    'title': 'The Prince',
-    'author': 'Machiavelli',
-    'pages': 222
+    title: 'The Prince',
+    author: 'Machiavelli',
+    pages: 222
   }
 ]
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary() {
+ for (const book of books) {
+   let bookDiv = document.createElement('div');
+   bookDiv.setAttribute('class', 'book');
+   bookDiv.textContent = book.title;
+   shelf1.appendChild(bookDiv);
+ }
+}
+
+
